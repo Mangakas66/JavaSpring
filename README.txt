b79fb6e1e044d76174821bab9c5609558d5c4e96
mangakas66 <yoannpons66@gmail.com>
2025-02-24 10:52:02 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+JavaSpring
+Gradle
+HelloWorld
---
+++ src/test/java/PackageName/NameOfThisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package PackageName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameOfThisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
