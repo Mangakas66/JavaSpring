f014532f0bef0bb85912e1c9b5879d584aa055b7
mangakas66 <yoannpons66@gmail.com>
2025-05-23 10:36:32 +0200

    Init

---
+++ src/test/java/com/example/Mediatek/MediatekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mediatek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
