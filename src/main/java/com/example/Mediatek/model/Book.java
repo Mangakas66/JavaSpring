b0beb616512877661f37ed94ed04b1f6d7bbfd14
mangakas66 <yoannpons66@gmail.com>
2025-05-23 15:00:01 +0200

    [ADD] Ajout de H2 en tant que BDD in-memory ainsi que d'un script pour charger la BDD et passage du fichier application.properties en yml

---
+++ src/main/java/com/example/Mediatek/model/Book.java
@@ -0,0 +1,43 @@
+package com.example.Mediatek.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book() {}
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into book (id, title, author, isbn) values(1, 'The little man and the old man', 'The philologist', '978-2-7654-0912-1');
+insert into book (id, title, author, isbn) values(2, 'The young vs the asmatic', 'Not a book', '978-2-7654-0912-2');
+insert into book (id, title, author, isbn) values(3, 'The marked one', 'Miss twitter', '978-2-7654-0912-3');
+insert into book (id, title, author, isbn) values(4, 'The strange rich man and the bored girl', 'A twilight fan', '978-2-7654-0912-4');
+insert into book (id, title, author, isbn) values(5, 'The old rich bored man who should not be left alone with some dna', 'The film is better', '978-2-7654-0912-5');
