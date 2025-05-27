4c65cef6b7c7b6e784b1ced733740c0231764f41
mangakas66 <yoannpons66@gmail.com>
2025-05-27 18:40:10 +0200

    [ADD] swagger

--- src/main/java/com/example/Mediatek/service/BookService.java
+++ src/main/java/com/example/Mediatek/service/BookService.java
@@ -4,15 +4,21 @@ import com.example.Mediatek.model.Book;
 import com.example.Mediatek.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/book")
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
 
+    @GetMapping(("/"))
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
